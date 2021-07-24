ca27bc6ae064ec409025f6907aa66c46c18d81d9
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2021-07-24 00:48:40 -0400

    fixing ch9 assignment 2 interface

--- ch9/files/animal.java
+++ ch9/files/animal.java
@@ -2,10 +2,10 @@ package com.CSC151.ch9.assignment2;
 
 public interface Animal {
     void move(String t);  // Type of movement is passed in
-    double getWeight();  // returns weight
+    double getWeight();   // returns weight
     void updateWeight(double x);
     void addHabitat(String y);
     String getHabitat();  // returns Habitat Type
-    String LatinName();  // returns Animal's Latin/Scientific Name
+    String LatinName();   // returns Animal's Latin/Scientific Name
     String toString();
 }
